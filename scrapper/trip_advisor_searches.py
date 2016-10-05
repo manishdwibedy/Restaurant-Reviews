30b0f73f7d1082bc3a02ae6e8407be28cfd4f6c5
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-10-05 14:15:05 -0700

    Renaming the file.

--- scrapper/trip_advisor_searches.py
+++ scrapper/trip_advisor_searches.py
@@ -30,25 +30,8 @@ class ScrapeTrip(object):
                 soup = BeautifulSoup(webpage)
                 saveSoupToFile(soup, join(self.directory, str(pageIndex) + '.txt'))
 
-    def getResLinks(self):
-        resLinks = []
-        for root, dirs, files in os.walk(self.directory, topdown=False):
-            for name in files:
-                file_path = os.path.join(root, name)
-                with open(file_path) as file:
-                    webpage = file.read().decode('utf-8')
-                soup = BeautifulSoup(webpage)
-
-                links = soup.findAll('a', attrs={'class': 'property_title'})
-
-                for link in links:
-                    resLinks.append(link.attrMap['href'])
-        saveToFile('\n'.join(resLinks), join('raw_data', 'res_links.txt'))
-
-
-
 if __name__ == '__main__':
-    ScrapeTrip().getResLinks()
+    ScrapeTrip().getSearchResults()
 
 
 
