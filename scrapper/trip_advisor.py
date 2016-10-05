b7edf2d916e3f64f5c6d43d4130c0a62aba063d6
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-10-05 14:13:45 -0700

    Now, saving the restaurants links to file.

--- scrapper/trip_advisor.py
+++ scrapper/trip_advisor.py
@@ -1,19 +1,20 @@
 from urllib import urlopen
 from BeautifulSoup import BeautifulSoup
-from util import saveToFile
+from util import saveSoupToFile, saveToFile
 from os.path import join
 from time import sleep
+import os
 
 class ScrapeTrip(object):
     def __init__(self):
         self.baseURL = 'http://www.tripadvisor.com'
-        self.directory = join('../raw_data', 'trip_search')
+        self.directory = join('raw_data', 'trip_search')
         self.url = 'https://www.tripadvisor.com/Restaurants-g32655-Los_Angeles_California.html'
 
     def getSearchResults(self):
         webpage = urlopen(self.url).read().decode('utf-8')
         soup = BeautifulSoup(webpage)
-        saveToFile(soup, join(self.directory, '1.txt'))
+        saveSoupToFile(soup, join(self.directory, '1.txt'))
 
         for pageIndex in range(2, 50):
             sleep(0.5)
@@ -27,10 +28,22 @@ class ScrapeTrip(object):
             if pageURL:
                 webpage = urlopen(pageURL).read().decode('utf-8')
                 soup = BeautifulSoup(webpage)
-                saveToFile(soup, join(self.directory, str(pageIndex) + '.txt'))
+                saveSoupToFile(soup, join(self.directory, str(pageIndex) + '.txt'))
 
     def getResLinks(self):
-        for root, dirs, files in os.walk(".", topdown=False):
+        resLinks = []
+        for root, dirs, files in os.walk(self.directory, topdown=False):
+            for name in files:
+                file_path = os.path.join(root, name)
+                with open(file_path) as file:
+                    webpage = file.read().decode('utf-8')
+                soup = BeautifulSoup(webpage)
+
+                links = soup.findAll('a', attrs={'class': 'property_title'})
+
+                for link in links:
+                    resLinks.append(link.attrMap['href'])
+        saveToFile('\n'.join(resLinks), join('raw_data', 'res_links.txt'))
 
 
 
--- scrapper/util.py
+++ scrapper/util.py
@@ -1,6 +1,10 @@
 
-def saveToFile(content, fileName):
+def saveSoupToFile(content, fileName):
     content = content.prettify("utf-8")
 
     with open(fileName, 'w') as file:
         file.write(str(content))
+
+def saveToFile(content, fileName):
+    with open(fileName, 'w') as file:
+        file.write(content)
