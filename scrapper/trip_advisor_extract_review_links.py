2445f89cf11f204f2b7891cfedb7918989ca34fd
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-10-05 18:42:24 -0700

    Extracting reviews links.

---
+++ scrapper/trip_advisor_extract_review_links.py
@@ -0,0 +1,52 @@
+from urllib import urlopen
+from BeautifulSoup import BeautifulSoup
+from util import saveSoupToFile
+from os.path import join
+from os import path, makedirs
+
+class ExtractReviewLinks(object):
+    def __init__(self):
+        self.baseURL = 'http://www.tripadvisor.com'
+
+    def saveRestaurantPages(self):
+        with open('raw_data/res_links.txt') as file:
+            res_links = file.readlines()
+
+        for link in res_links:
+            link = self.baseURL + link
+
+            webpage = urlopen(link).read().decode('utf-8')
+            soup = BeautifulSoup(webpage)
+
+            res_name_h1 = soup.findAll('h1', attrs={'id': 'HEADING'})[0]
+
+            res_name = str(res_name_h1.contents[2]).strip()
+
+            directory_location = join('raw_data', 'trip_restaurant', res_name)
+
+            if not path.exists(directory_location):
+                makedirs(directory_location)
+
+            saveSoupToFile(soup, join(directory_location, '1.txt'))
+
+            for pageIndex in range(5, 55):
+                try:
+                    links = soup.findAll('div', attrs={'class': 'pageNumbers'})[0]
+                    page = links.findAll('a', attrs={'data-page-number': pageIndex})[0]
+
+                    for attr, value in page.attrs:
+                        if attr == 'href':
+                            pageURL = self.baseURL + value
+                            break
+                    if pageURL:
+                        webpage = urlopen(pageURL).read().decode('utf-8')
+                        soup = BeautifulSoup(webpage)
+                        saveSoupToFile(soup, join(directory_location, str(pageIndex) + '.txt'))
+                except:
+                    print 'Completed index ' + str(pageIndex)
+                    break
+
+
+
+if __name__ == '__main__':
+    ExtractReviewLinks().saveRestaurantPages()
