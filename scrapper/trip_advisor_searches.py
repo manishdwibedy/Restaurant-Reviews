c81b19bfc6f33eb7d314526b647b7750e3d617a2
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-10-12 13:26:06 -0700

    Tested the indexing of the search results in the solr collection.

--- scrapper/trip_advisor_searches.py
+++ scrapper/trip_advisor_searches.py
@@ -3,17 +3,25 @@ from BeautifulSoup import BeautifulSoup
 from util import saveSoupToFile
 from os.path import join
 from time import sleep
+from solr import connection, index
+from solr import constant
 
 class ScrapeTrip(object):
     def __init__(self):
         self.baseURL = 'http://www.tripadvisor.com'
         self.directory = join('raw_data', 'trip_search')
         self.url = 'https://www.tripadvisor.com/Restaurants-g32655-Los_Angeles_California.html'
+        self.saveToFile = False
+        self.conn = connection.get_connection()
 
     def getSearchResults(self):
         webpage = urlopen(self.url).read().decode('utf-8')
         soup = BeautifulSoup(webpage)
-        saveSoupToFile(soup, join(self.directory, '1.txt'))
+        if self.saveToFile:
+            saveSoupToFile(soup, join(self.directory, '1.txt'))
+
+        self.searches = []
+        self.searches.append(self.url)
 
         for pageIndex in range(2, 50):
             sleep(0.5)
@@ -25,9 +33,21 @@ class ScrapeTrip(object):
                     pageURL = self.baseURL + value
                     break
             if pageURL:
+                self.searches.append(pageURL)
                 webpage = urlopen(pageURL).read().decode('utf-8')
                 soup = BeautifulSoup(webpage)
-                saveSoupToFile(soup, join(self.directory, str(pageIndex) + '.txt'))
+                if self.saveToFile:
+                    saveSoupToFile(soup, join(self.directory, str(pageIndex) + '.txt'))
+        self.saveSearchResults()
+
+    def saveSearchResults(self):
+        documents = []
+        for link in self.searches:
+            documents.append({
+                'site': 'Trip Advisor',
+                'url': link
+            })
+        index.index(connection=self.conn, collection=constant.SEARCHES_COLLECTION,document=documents)
 
 if __name__ == '__main__':
     ScrapeTrip().getSearchResults()
--- solr/constant.py
+++ solr/constant.py
@@ -5,3 +5,4 @@
 
 REVIEWS_COLLECTION = 'reviews'
 RESTAURANTS_COLLECTION = 'restaurants'
+SEARCHES_COLLECTION = 'RR_searches'
