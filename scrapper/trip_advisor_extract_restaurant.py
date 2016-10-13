7956dac993730073ee6721d98819d00bcb6ba2da
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-10-12 21:09:54 -0700

    If reviews, rating or cuisines is not available, set N.A.

--- scrapper/trip_advisor_extract_restaurant.py
+++ scrapper/trip_advisor_extract_restaurant.py
@@ -2,7 +2,7 @@ import os
 from os.path import join
 from BeautifulSoup import BeautifulSoup
 from util import saveToFile
-from solr import connection, query, constant, index
+from solr import connection, query, constant, index, delete
 from urllib import urlopen
 
 class ExtractRestaurantLinks(object):
@@ -64,52 +64,58 @@ class ExtractRestaurantLinks(object):
         for restaurant in results.findAll('div'):
             if 'listing' in restaurant.attrMap['class']:
 
+                #name
+                name = str(link.contents[0]).strip()
+
+                link = str(self.baseURL + link.attrMap['href'])
+
                 # Link to the restaurant
                 link = restaurant.findAll('a', attrs={'class': 'property_title'})[0]
 
                 # Number of reviews
-                reviewCountSpan = restaurant.findAll('span', attrs={'class': 'reviewCount'})
-                reviewCount = str(reviewCountSpan[0].findAll('a')[0].contents[0]).strip()
+                try:
+                    reviewCountSpan = restaurant.findAll('span', attrs={'class': 'reviewCount'})
+                    reviewCount = str(reviewCountSpan[0].findAll('a')[0].contents[0]).strip()
+                except:
+                    reviewCount = 'N.A.'
 
                 # Rating
-                ratingImage = restaurant.findAll('img', attrs={'class': 'sprite-ratings'})[0]
-                rating = str(ratingImage.attrMap['alt'])
+                try:
+                    ratingImage = restaurant.findAll('img', attrs={'class': 'sprite-ratings'})[0]
+                    rating = str(ratingImage.attrMap['alt'])
+                except:
+                    rating = 'N.A.'
+
+                #Price Range
                 try:
                     price_range_span = restaurant.findAll('span', attrs={'class': 'price_range'})[0].findAll('span')[0]
                     price_range = str(price_range_span.contents[0])
                 except:
                     price_range = 'N.A.'
 
+                #Cuisines
                 cuisines_list = restaurant.findAll('div', attrs={'class': 'cuisines'})
                 cuisines = []
                 if len(cuisines_list) > 0:
                     for cuisine in cuisines_list[0].findAll('a', attrs={'class': 'cuisine'}):
                         cuisines.append(str(cuisine.contents[0]))
+                    cuisine = ', '.join(cuisines)
+                else:
+                    cuisine = 'N.A.'
 
                 restaurant_info.append({
-                    'name': str(link.contents[0]).strip(),
-                    'link': str(self.baseURL + link.attrMap['href']),
+                    'name': name,
+                    'link': link,
                     'reviewCount': reviewCount,
                     'res_rating': rating,
                     'price_range': price_range,
-                    'cuisines': ','.join(cuisines)
+                    'cuisines': cuisine
                 })
                 pass
 
-
-        # for index, link in enumerate(links):
-        #     restaurant_data.append({
-        #         'URL': link.attrMap['href'],
-        #         'reviewCount': str(reviewCountSpan[index].findAll('a')[0].contents[0]).strip()
-        #     })
-        #     # link_list.append(link.attrMap['href'])
-        #     # reviewCount_list.append(str(reviewCountSpan[index].findAll('a')[0].contents[0]).strip())
         return restaurant_info
 
 
 if __name__ == '__main__':
-    # connection = connection.get_connection()
-    # docs = [{"id":"1", "name":"a"},{"id":"2","name":"b"}]
-    #
-    # index.index(connection, constant.RESTAURANTS_COLLECTION,docs)
+    #delete.deleteRestaurants()
     ExtractRestaurantLinks().extractLinks()
